2d51ed47b2f7c8437f6c25d1a6b7cef58b7031e1
Kor-jiwon <opoog9999@naver.com>
2024-04-09 12:33:38 +0900

    wsgi set

--- config/wsgi.py
+++ config/wsgi.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
 
 import os
 
+from .settings.base import *
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.base')
