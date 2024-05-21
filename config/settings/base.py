29694807b377027c21850efc10914ed9453ee1d6
Kor-jiwon <opoog9999@naver.com>
2024-05-22 02:34:06 +0900

    crontest

--- config/settings/base.py
+++ config/settings/base.py
@@ -43,8 +43,8 @@ INSTALLED_APPS = [
 CRONJOBS = [
     ('30 16 * * *', 'cron.cron.load430', 'cron/log/cron.log'),
     ('20 18 * * *', 'cron.cron.load620', 'cron/log/cron.log'),
-    ('55 18 * * *', 'cron.cron.mstatu', 'cron/log/cron01.log')
-    ('35 2 * * *', 'cron.cron.mstatu', 'cron/log/cron01.log')
+    ('55 18 * * *', 'cron.cron.mstatu', 'cron/log/cron01.log'),
+    ('37 2 * * *', 'cron.cron.mstatu', 'cron/log/cron01.log')
 ]
 
 MIDDLEWARE = [
