6c7544286245b90d0a1879f1cfa0ec425fb0e16e
Kor-jiwon <opoog9999@naver.com>
2024-05-28 17:20:47 +0900

    crolling update

--- pybo/tasks.py
+++ pybo/tasks.py
@@ -6,21 +6,18 @@ scheduler = BackgroundScheduler({'apscheduler.timezone': 'Asia/seoul'})
 
 def load430():
     for card in PreCard.objects.filter(moving_date__date=timezone.now().date(), time="8, 9교시"):
-        a = Card(stu=card.stu, to_=card.to, why=card.why, moving_date=card.moving_date)
-        a.save()
+        PreCard.objects.create(stu=card.stu, to_=card.to, why=card.why, moving_date=card.moving_date)
 
 def load620():
     for card in PreCard.objects.filter(moving_date__date=timezone.now().date(), time="1차야자"):
-        a = Card(stu=card.stu, to_=card.to, why=card.why, moving_date=card.moving_date)
-        a.save()
+        PreCard.objects.create(stu=card.stu, to_=card.to, why=card.why, moving_date=card.moving_date
 
 def mstatu():
     students = Student.objects.all()
     for student in students:
-        a = Card(to='재실', why='초기상태', stu=student, moving_date=timezone.now())
-        a.save()
+        Card.objects.create(to='재실', why='초기상태', stu=student, moving_date=timezone.now())
 
 scheduler.add_job(load430, 'cron', day_of_week='mon-fri', hour=16, minute=30)
 scheduler.add_job(load620, 'cron', day_of_week='mon-fri', hour=18, minute=30)
 scheduler.add_job(mstatu, 'cron', day_of_week='mon-fri', hour=0, minute=0)
-scheduler.add_job(mstatu, 'cron', hour=17, minute=15)
+scheduler.add_job(mstatu, 'cron', hour=17, minute=25)
