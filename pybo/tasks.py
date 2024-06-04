c6a2ee279bf7e563f2c661428348c5bab31820d3
Kor-jiwon <63719590+Kor-jiwon@users.noreply.github.com>
2024-06-04 17:18:20 +0900

    tasks.py 업데이트

--- pybo/tasks.py
+++ pybo/tasks.py
@@ -1,7 +1,20 @@
-from pybo.models import *
-from django.utils import timezone
-from django_apscheduler.jobstores import DjangoJobStore
+
+import logging
+
+from django.config.settings import prod
+
+from apscheduler.schedulers.blocking import BlockingScheduler
 from apscheduler.schedulers.background import BackgroundScheduler
+from apscheduler.triggers.cron import CronTrigger
+from django.utils import timezone
+from django.core.management.base import BaseCommand
+from django_apscheduler.jobstores import DjangoJobStore
+from django_apscheduler.models import DjangoJobExecution
+from django_apscheduler import util
+
+logger = logging.getLogger(__name__)
+
+
 def load430():
     for card in PreCard.objects.filter(moving_date__date=timezone.now().date(), time="8, 9교시"):
         PreCard.objects.create(stu=card.stu, to_=card.to, why=card.why, moving_date=card.moving_date)
@@ -15,10 +28,43 @@ def mstatu():
     for student in students:
         Card.objects.create(to='재실', why='초기상태', stu=student, moving_date=timezone.now())
 
-scheduler = BackgroundScheduler(daemon=True, timezone='Asia/Seoul')
-scheduler.add_jobstore(DjangoJobStore(), 'djangojobstore')
-scheduler.add_job(load430, 'cron', day_of_week='mon-fri', hour=16, minute=30)
-scheduler.add_job(load620, 'cron', day_of_week='mon-fri', hour=18, minute=30)
-scheduler.add_job(mstatu, 'cron', day_of_week='mon-fri', hour=0, minute=0)
-scheduler.add_job(mstatu, 'cron', hour=15, minute=30)
-scheduler.start()
+@util.close_old_connections
+def delete_old_job_executions(max_age=604_800):
+DjangoJobExecution.objects.delete_old_job_executions(max_age)
+
+class Command(BaseCommand):
+  help = "Runs APScheduler."
+
+  def handle(self, *args, **options):
+    scheduler = BackgroundScheduler(timezone=TIME_ZONE)
+    scheduler.add_jobstore(DjangoJobStore(), "default")
+
+    scheduler.add_job(
+      mstatu,
+      trigger=CronTrigger(hour=“17”, minute = “25”),
+      id="reset_card",  # The `id` assigned to each job MUST be unique
+      max_instances=1,
+      replace_existing=True,
+    )
+    logger.info("Added job 'reset_card'.")
+
+    scheduler.add_job(
+      delete_old_job_executions,
+      trigger=CronTrigger(
+        day_of_week="mon", hour="00", minute="00"
+      ),  # Midnight on Monday, before start of the next work week.
+      id="delete_old_job_executions",
+      max_instances=1,
+      replace_existing=True,
+    )
+    logger.info(
+      "Added weekly job: 'delete_old_job_executions'."
+    )
+
+    try:
+      logger.info("Starting scheduler...")
+      scheduler.start()
+    except KeyboardInterrupt:
+      logger.info("Stopping scheduler...")
+      scheduler.shutdown()
+      logger.info("Scheduler shut down successfully!")
