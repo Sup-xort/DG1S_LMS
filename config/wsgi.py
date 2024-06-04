058a25294e7a05d7507799de86583534398fa8c3
Kor-jiwon <63719590+Kor-jiwon@users.noreply.github.com>
2024-06-04 17:23:28 +0900

    wsgi.py 업데이트

--- config/wsgi.py
+++ config/wsgi.py
@@ -11,7 +11,6 @@ import os
 
 from .settings.base import *
 from django.core.wsgi import get_wsgi_application
-from pybo.tasks import scheduler
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings.base')
 
