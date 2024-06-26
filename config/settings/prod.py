fe33cb0d778e210879cc960415f00219b6885cdf
Kor-jiwon <opoog9999@naver.com>
2024-06-27 02:13:01 +0900

    domain

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -4,3 +4,5 @@ ALLOWED_HOSTS = ['34.16.149.51', 'dg1s.o-r.kr']
 STATIC_ROOT = BASE_DIR / 'static/'
 STATICFILES_DIRS = []
 DEBUG = False
+MIDDLEWARE += ['django.middleware.clickjacking.XFrameOptionsMiddleware', ]
+X_FRAME_OPTIONS = 'http://dg1s.o-r.kr/'
