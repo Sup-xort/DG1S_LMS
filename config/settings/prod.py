223f6a65c75cea88ec0c6e114c9f03398ac876ec
Kor-jiwon <opoog9999@naver.com>
2024-06-27 09:58:23 +0900

    domain

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -1,6 +1,6 @@
 from .base import *
 
-ALLOWED_HOSTS = ['34.16.149.51', 'dg1s.o-r.kr']
+ALLOWED_HOSTS = ['34.64.118.189', 'dg1s.o-r.kr']
 STATIC_ROOT = BASE_DIR / 'static/'
 STATICFILES_DIRS = []
 DEBUG = False
