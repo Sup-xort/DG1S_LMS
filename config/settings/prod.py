e63abe590f310bd1d25e2ec010491ce8e6ead70d
Kor-jiwon <opoog9999@naver.com>
2024-07-08 00:14:11 +0900

    특별실 신청

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -1,8 +1,8 @@
 from .base import *
 
-ALLOWED_HOSTS = ['34.64.118.189', 'www.dg1s.o-r.kr']
+ALLOWED_HOSTS = ['34.64.118.189', 'www.dg1s.o-r.kr', 'dg1s.o-r.kr']
 STATIC_ROOT = BASE_DIR / 'static/'
 STATICFILES_DIRS = []
-#DEBUG = False
+DEBUG = False
 MIDDLEWARE += ['django.middleware.clickjacking.XFrameOptionsMiddleware', ]
 X_FRAME_OPTIONS = 'http://www.dg1s.o-r.kr/'
