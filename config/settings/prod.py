3e6dbd51839b98aa8bc91047b3455d40f132b188
Kor-jiwon <63719590+Kor-jiwon@users.noreply.github.com>
2024-07-08 22:33:53 +0900

    Update prod.py

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -3,6 +3,6 @@ from .base import *
 ALLOWED_HOSTS = ['35.188.118.24', '12th.dg1s.o-r.kr']
 STATIC_ROOT = BASE_DIR / 'static/'
 STATICFILES_DIRS = []
-DEBUG = False
+#DEBUG = False
 MIDDLEWARE += ['django.middleware.clickjacking.XFrameOptionsMiddleware', ]
 X_FRAME_OPTIONS = 'http://12th.dg1s.o-r.kr/'
