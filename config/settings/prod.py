3c9503e390797157d9d2088485a2d4a482e37c81
Kor-jiwon <opoog9999@naver.com>
2024-12-18 14:13:23 +0900

    font update, GUI

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -3,6 +3,6 @@ from .base import *
 ALLOWED_HOSTS = ['10.72.120.132', 'dg1s.pythonanywhere.com']
 STATIC_ROOT = BASE_DIR / 'static/'
 STATICFILES_DIRS = []
-DEBUG = False
+DEBUG = True
 MIDDLEWARE += ['django.middleware.clickjacking.XFrameOptionsMiddleware', ]
 X_FRAME_OPTIONS = 'http://www.dg1s.o-r.kr/'
